41205267d947c6c8c5870c3823bc63a793dd8174
mohanfyi <mohanfyi@users.noreply.github.com>
2015-07-23 17:20:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var PORT = 8080;
 // App
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello world\nHows it going?');
+  res.send('Hello world...Hows it going?...Is today Thursday?');
 });
 app.listen(PORT);
 console.log('Running on http://localhost:' + PORT);
