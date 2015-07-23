fef1edbffb3c3dba79abb47f19d0c5116f6e6eaa
mohanfyi <mohanfyi@users.noreply.github.com>
2015-07-23 18:08:39 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@
 # Script to fetch latest changes from version control and refresh pods
 
 cd ..
-docker build -t gcr.io/prjmonday/july23sample1version3  .
-gcloud docker push gcr.io/prjmonday/july23sample1version3
+docker build -t gcr.io/prjmonday/demoapp  .
+gcloud docker push gcr.io/prjmonday/demoapp
 
-kubectl rolling-update sample --image=gcr.io/prjmonday/july23sample1version3
+kubectl rolling-update sample --image=gcr.io/prjmonday/demoapp
