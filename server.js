133f85da47c6fc940999de6f06f9ae6cec7702b0
mohanfyi <mohanfyi@users.noreply.github.com>
2015-07-23 18:09:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var PORT = 8080;
 // App
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello world...Hows it going?...Is today Thursday?..Checking whether same image name can be used!');
+  res.send('Hello world...Hows it going?...Is today Thursday?..Checking whether same image name can be used...name is demoapp!');
 });
 app.listen(PORT);
 console.log('Running on http://localhost:' + PORT);
