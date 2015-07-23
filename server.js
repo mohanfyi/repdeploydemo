540eba8a6379c2e78daf156bec455c2135642388
mohan <mohan@co.com>
2015-07-23 12:58:00 +0000

    IP address

--- server.js
+++ server.js
@@ -4,7 +4,24 @@ var PORT = 8080;
 // App
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello world...Hows it going?...Is today Thursday?..Checking whether same image name can be used...name is demoapp!');
+  var response = 'Hello world...Hows it going?...Is today Thursday?..Checking whether same image name can be used...name is demoapp!';
+  response+='........[This request was served by:' + addresses + ']';
+  res.send(response);
 });
 app.listen(PORT);
 console.log('Running on http://localhost:' + PORT);
+
+var os = require('os');
+
+var interfaces = os.networkInterfaces();
+var addresses = [];
+for (var k in interfaces) {
+    for (var k2 in interfaces[k]) {
+        var address = interfaces[k][k2];
+        if (address.family === 'IPv4' && !address.internal) {
+            addresses.push(address.address);
+        }
+    }
+}
+
+console.log(addresses);
