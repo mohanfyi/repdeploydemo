8bf636833214b28a6621e94d520059eccb3c9ddf
mohanfyi <mohanfyi@users.noreply.github.com>
2015-07-29 11:16:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var PORT = 8080;
 // App
 var app = express();
 app.get('/', function (req, res) {
-  var response = 'Greetings Jenkins on July 28 12:25 PM!!...';
+  var response = 'Greetings Jenkins on July 29!!...';
   response+='........[This request was served by:' + addresses + ':' + PORT + ']';
   res.send(response);
 });
